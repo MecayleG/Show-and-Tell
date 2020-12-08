185678d070a66c40b0ad9573eeeabd7182b63343
mduduziavatar <mduduziavatar@gmail.com>
2020-12-08 16:55:09 +0200

    saving progress

--- public/show.js
+++ public/show.js
@@ -1,11 +1,8 @@
-< div > Teachable Machine Image Model < /div> <
+< div > Teachable Machine Image Model <
+    /div> <
 button type = "button"
-onclick = "init()" > Start < /button> <
-div id = "webcam-container" > < /div> <
-div id = "label-container" > < /div> <
-script src = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js" > < /script> <
-script src = "https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js" > < /script> <
-script type = "text/javascript" >
+onclick = "init()" > Start < /button> <div id = "webcam-container"> </div > < div id = "label-container" > < /div> <script src = "https:/ / cdn.jsdelivr.net / npm / @tensorflow / tfjs @1 .3 .1 / dist / tf.min.js "> </script> <script src = "
+https: //cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js"> </script> <script type = "text/javascript">
     // More API functions here:
     // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
