8a44e5329b01072a7e5c8cf8a45a216832099f1a
mduduziavatar <mduduziavatar@gmail.com>
2020-12-09 10:41:44 +0200

    saving layout

--- public/show.js
+++ public/show.js
@@ -1,13 +1,8 @@
-< div > Teachable Machine Image Model <
-    /div> <
-button type = "button"
-onclick = "init()" > Start < /button> <div id = "webcam-container"> </div > < div id = "label-container" > < /div> <script src = "https:/ / cdn.jsdelivr.net / npm / @tensorflow / tfjs @1 .3 .1 / dist / tf.min.js "> </script> <script src = "
-https: //cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js"> </script> <script type = "text/javascript">
-    // More API functions here:
-    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
-    // the link to your model provided by Teachable Machine export panel
-    const URL = "./my_model/";
+// the link to your model provided by Teachable Machine export panel
+const URL = "./my_model/";
 
 let model, webcam, labelContainer, maxPredictions;
 
@@ -53,5 +48,4 @@ async function predict() {
             prediction[i].className + ": " + prediction[i].probability.toFixed(2);
         labelContainer.childNodes[i].innerHTML = classPrediction;
     }
-} <
-/script>
+}
